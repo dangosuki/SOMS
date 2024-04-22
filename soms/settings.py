597468b843a9fb64fcf28dbf3e54ee35eee9f866
camote <116661753+dangosuki@users.noreply.github.com>
2024-04-22 20:09:44 +0800

    Update settings.py

--- soms/settings.py
+++ soms/settings.py
@@ -149,6 +149,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+CSRF_TRUSTED_ORIGINS=[*]
+                      
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
