fbceb20d46e76c9e0c2325ba8521153ce2c2ddae
camote <116661753+dangosuki@users.noreply.github.com>
2024-04-22 21:29:34 +0800

    Finales

--- soms/settings.py
+++ soms/settings.py
@@ -146,12 +146,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/images/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-    os.path.join(BASE_DIR, 'images')
 ]
 
 
