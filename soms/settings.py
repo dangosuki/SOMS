3a9669af61f7259e789ddb172df4fea29548522f
camote <116661753+dangosuki@users.noreply.github.com>
2024-04-24 22:26:48 +0800

    please static work

--- soms/settings.py
+++ soms/settings.py
@@ -142,11 +142,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/images/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATICFILES_DIRS = [
